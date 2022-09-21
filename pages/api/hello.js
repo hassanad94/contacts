63523d3b202e48be64ddd196f5154947f6355e41
unknown <hassanad94@gmail.com>
2022-09-21 21:00:24 +0200

    sqlite setup

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,19 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { sqlite3 } from "sqlite3";
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const sqlite3 = require("sqlite3").verbose();
+  const db = new sqlite3.Database(
+    "./contacts.db",
+    sqlite3.OPEN_READWRITE,
+    (err) => {
+      if (err) return console.error(err.message);
+
+      return console.log("db connected");
+    }
+  );
+
+  db.all("SELECT * FROM person", [], (err, rows) => {
+    if (err) return console.error(err.message);
+    res.status(200).json(rows);
+  });
 }
